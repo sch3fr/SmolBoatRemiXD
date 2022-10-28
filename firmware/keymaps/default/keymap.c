042936c3cc7bb29208769bed615a3148cb488843
sch3fr <sch3fr@gmail.com>
2022-10-28 18:55:02 +0200

    swapped layers, added ()

--- firmware/keymaps/default/keymap.c
+++ firmware/keymaps/default/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |SftEnt|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | LCtrl| Func | LGUI | LAlt | Lower|    Space    | Raise| Left | Down |  Up  |Right |
+ * | LCtrl| Func | LGUI | LAlt | Raise|    Space    | Lower| Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 
@@ -28,25 +28,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    CZ_UACU, 
 		KC_ESC,  KC_A,      KC_S,    KC_D,    KC_F,    KC_G,      KC_H,       KC_J,    KC_K,    KC_L,    CZ_URNG, CZ_SECT, 
 		KC_LSFT, KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,      KC_N,       KC_M,    CZ_COMM, CZ_DOT,  CZ_MINS, KC_SFTENT, 
-		KC_RCTL, MO(_FUNC), KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
+		KC_RCTL, MO(_FUNC), KC_LGUI, KC_LALT, MO(_RAISE), KC_SPC, MO(_LOWER), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
  * |-----------------------------------------------------------------------------------|
- * |      |      |      |      |      |      |      |      |      |      |PrtScn|  Del |
+ * |      |      |      |      |      |      |      |      |      |  Del |PrtScn|  ()  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |  \   |      | 
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Func |      |      | Lower|             | Raise| Home | PgDn | PgUp |  End |
+ * |      | Func |      |      | Raise|             | Lower| Home | PgDn | PgUp |  End |
  * `-----------------------------------------------------------------------------------'
  */
 
 	[_RAISE] = LAYOUT_ortho_5x12(
 		KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_DEL, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_PSCR, CZ_RPRN, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BSLS, KC_TRNS, 
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END),
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |   #  |   &  |   @  |   {  |   }  |   ^  |   <  |   >  |   *  |      | 
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Func |      |      | Lower|             | Raise|      |      |      |      |
+ * |      | Func |      |      | Raise|             | Lower|      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 
