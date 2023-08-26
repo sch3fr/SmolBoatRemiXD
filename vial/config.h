0b3d80e27f7b6ea92ceb5ce3a0b0ad3c3127f754
sch3fr <sch3fr@gmail.com>
2023-08-26 10:25:05 +0200

    Finished Vial support

---
+++ vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD5, 0x9C, 0xFA, 0x11, 0xAD, 0x8E, 0x20, 0xB3}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 13 }
